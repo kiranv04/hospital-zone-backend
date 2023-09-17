f0919f137425b335f14699af4d9904f092c534b2
Kiran <kiran.bv@uvaaninfotech.com>
2023-09-17 20:12:25 +0530

    Created new project files with dependencies

---
+++ hospital-zone-parent/src/main/java/com/hospitalzone/hospitalzoneparent/HospitalZoneParentApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalzone.hospitalzoneparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalZoneParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalZoneParentApplication.class, args);
+	}
+
+}
